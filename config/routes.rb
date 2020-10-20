2d98d6a01455c92621a2ece01ee6ea77e09481d6
Yassine Chaqroun <ychaqroun@gmail.com>
2020-10-20 09:53:20 +0100

    Rails stupid coach updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'ask', to: 'pages#question'
-  get 'answer', to: 'pages#answer'
+  root to: 'questions#home'
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
